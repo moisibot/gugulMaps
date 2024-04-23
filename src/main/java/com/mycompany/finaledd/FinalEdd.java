12fdc3090c78ea15440264213f66c189161ceac2
moisibot <moisesvasquez201930195@cunoc.edu.gt>
2024-04-23 01:05:11 -0600

    interfaz beta

---
+++ src/main/java/com/mycompany/finaledd/FinalEdd.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.finaledd;
+
+import Front.Principal;
+
+public class FinalEdd {
+
+    public static void main(String[] args) {
+       Principal ide = new Principal();
+        ide.setVisible(true);
+
+    }
+}
